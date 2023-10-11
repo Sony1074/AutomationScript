cbdba30918fc0218ffd94309573218059543a257
Ayush <Ayush@LAPTOP-33SQBDS2>
2023-10-11 15:29:28 +0530

    Add project files.

---
+++ Sony_1/Sony_1.cs
@@ -0,0 +1,24 @@
+namespace Sony_1
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Globalization;
+	using System.Text;
+	using Skyline.DataMiner.Automation;
+	
+	/// <summary>
+	/// Represents a DataMiner Automation script.
+	/// </summary>
+	public class Script
+	{
+		/// <summary>
+		/// The script entry point.
+		/// </summary>
+		/// <param name="engine">Link with SLAutomation process.</param>
+		public void Run(IEngine engine)
+		{
+			engine.GenerateInformation("Hello world!");
+	
+		}
+	}
+}
